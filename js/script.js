5b8f1d8bd2be5abdbef75aca1f05e15db46431ee
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-24 15:28:40 +0500

    middle

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let elResult = document.querySelector(".movies__result")
 let elList = document.querySelector(".movies__list")
+let elInfo = document.querySelector(".info")
+let elTrilerLink = "https://www.youtube.com/watch?v="
 
 elResult.textContent = movies.length
 
 
-const renderFilms = function(filmsArray, element){
+const renderFilms = function(filmsArray, element, elInfoElement){
   filmsArray.forEach(movie => {
       //CREATE
   let newItem = document.createElement('li')
@@ -19,9 +21,10 @@ const renderFilms = function(filmsArray, element){
   let newCardRatingImg = document.createElement('img')
   let newCardRating = document.createElement('p')
   let newBtnSection = document.createElement('div')
-  let newTrailer = document.createElement('button')
+  let newTrailerLink = document.createElement('a')
   let newInfo = document.createElement('button')
   let newBookmark = document.createElement('button')
+
 
   //SET ATTRIBUTE
   newItem.setAttribute('class', 'movies__item')
@@ -40,17 +43,43 @@ const renderFilms = function(filmsArray, element){
   newCardRatingImg.setAttribute('width', '20px')
   newCardRating.setAttribute('class', 'card-text')
   newBtnSection.setAttribute('class', 'btn-section')
-  newTrailer.setAttribute('class', 'btn-trailer btn')
+  newTrailerLink.setAttribute('class', 'btn-trailer btn')
+  newTrailerLink.setAttribute('href', elTrilerLink + movie.youtubeId)
   newInfo.setAttribute('class', 'btn-infoo btn')
   newBookmark.setAttribute('class', 'btn-bookmark btn')
+
 
   //TEXT CONTENT
   newCardTitle.textContent = movie.title
   newCardYear.textContent = movie.year
   newCardRating.textContent = movie.imdbRating
-  newTrailer.textContent = 'Watch trailer'
+  newTrailerLink.textContent = 'Watch trailer'
+
   newInfo.textContent = 'More info'
   newBookmark.textContent = 'Bookmark'
+
+
+
+  // BTN
+  newInfo.onclick = function(){
+    elInfo.style.display = "inline"
+
+
+    let newBigImg = document.createElement('img')
+    let newSummary = document.createElement('p')
+
+    newSummary.style.color = "#fff"
+
+    newBigImg.setAttribute('class', 'big-img')
+    newBigImg.setAttribute('src', movie.smallThumbnail)
+
+    newSummary.textContent = movie.summary
+
+    elInfoElement.appendChild(newBigImg)
+    elInfoElement.appendChild(newSummary)
+  }
+
+
 
   //APPEND
   element.appendChild(newItem)
@@ -65,10 +94,10 @@ const renderFilms = function(filmsArray, element){
   newCardRatingSection.appendChild(newCardRatingImg)
   newCardRatingSection.appendChild(newCardRating)
   newCardBody.appendChild(newBtnSection)
-  newBtnSection.appendChild(newTrailer)
+  newBtnSection.appendChild(newTrailerLink)
   newBtnSection.appendChild(newInfo)
   newBtnSection.appendChild(newBookmark)
   })
 }
 
-renderFilms(movies, elList)
+renderFilms(movies, elList, elInfo)
